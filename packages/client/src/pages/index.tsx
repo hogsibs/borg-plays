42dd092e4b970e62cb61e253ac89b8b62a4871df
Melody Universe <melody.universe@microsoft.com>
2022-09-23 12:22:24 -0700

    set up host and lifted eslint / tsconfig to root

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,7 +1,15 @@
-import React, { FunctionComponent } from "react";
 import type { HeadFC, PageProps } from "gatsby";
+import React, { FunctionComponent, useCallback } from "react";
+import { io } from "socket.io-client";
 
 export const Head: HeadFC = () => <title>Borg Plays - Home</title>;
+
+const Connector: FunctionComponent = () => {
+  const connect = useCallback(() => {
+    io("http://localhost:8001/");
+  }, []);
+  return <button onClick={connect}>Connect</button>;
+};
 
 const IndexPage: FunctionComponent<PageProps> = () => {
   return (
@@ -9,6 +17,7 @@ const IndexPage: FunctionComponent<PageProps> = () => {
       <h1>Borg Plays</h1>
       <section>
         <p>Game will display here.</p>
+        <Connector />
       </section>
     </main>
   );
---
+++ packages/host/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => console.log("a user disconnected"));
+});
+
+const port = 8001;
+server.listen(port, () => console.log(`listening on port ${port}`));
