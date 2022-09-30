b612878f7423f06377022cde89becb15993afb87
Melody Universe <melody.universe.2021@gmail.com>
2022-09-29 21:42:23 -0700

    added sound

--- packages/chip8-emulator/src/Emulator.tsx
+++ packages/chip8-emulator/src/Emulator.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import bindKeyboard from "./bind-keyboard";
 import { screenHeight, screenWidth } from "./constants";
 import executeNextOperation from "./execute-next-operation";
@@ -10,21 +10,56 @@ import setEmulatedInterval from "./set-emulated-interval";
 
 export default () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [audioContext] = useState(new AudioContext());
+
+  const beep = useCallback(
+    (time: number) => {
+      const oscillator = audioContext.createOscillator();
+      oscillator.type = "sine";
+      oscillator.frequency.setValueAtTime(261.63, audioContext.currentTime);
+
+      const maxLevel = 0.5;
+      const attackTime = time * 0.1;
+      const decayTime = time * 0.3;
+      const sustainLevel = 0.7;
+      const releaseTime = time * 0.2;
+
+      const now = audioContext.currentTime;
+      const beepGain = audioContext.createGain();
+      beepGain.connect(audioContext.destination);
+      beepGain.gain.setValueAtTime(0, 0);
+      beepGain.gain.linearRampToValueAtTime(maxLevel, now + attackTime);
+      beepGain.gain.linearRampToValueAtTime(
+        maxLevel * sustainLevel,
+        now + attackTime + decayTime
+      );
+      beepGain.gain.setValueAtTime(
+        maxLevel * sustainLevel,
+        now + (1 - releaseTime) * time
+      );
+      beepGain.gain.linearRampToValueAtTime(0, now + time);
+      oscillator.connect(beepGain);
+
+      oscillator.start();
+      oscillator.stop(audioContext.currentTime + time);
+    },
+    [audioContext]
+  );
   const [canvasContext, setCanvasContext] =
     useState<CanvasRenderingContext2D | null>(null);
+
   useEffect(() => {
     if (canvasRef.current !== null && !canvasContext) {
       setCanvasContext(canvasRef.current.getContext("2d"));
     }
   }, []);
+
   useEffect(() => {
     if (!canvasContext) return;
 
     const c8 = initializeC8({
       setSoundTimerToVx: (_, c8, code) => {
-        console.log(
-          `beeping for ${Math.round((c8.registers[getX(code)] * 1000) / 60)}ms`
-        );
+        beep(c8.registers[getX(code)] / 60);
       },
     });
     const unbindKeyboard = bindKeyboard(c8);
